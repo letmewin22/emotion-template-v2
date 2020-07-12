20e243a3a4ef7c84f4dd40047f69f51c7e27259c
letmewin22 <46356822+letmewin22@users.noreply.github.com>
2020-07-12 19:02:33 +0300

    v2

---
+++ gulp/foldersName.js
@@ -0,0 +1,10 @@
+const projectName = ''
+
+const foldersName = {
+  projectFolder: 'build',
+  // projectFolder: 'C:/MAMP/htdocs/' + projectName + '/wp-content/themes/' + projectName,
+  // projectFolder: require('path').basename(__dirname)
+  sourceFolder: 'src'
+}
+
+module.exports = foldersName
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const webpackReal = require('webpack')
+const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+
+function createConfig(env) {
+  let isProduction,
+    webpackConfig
+
+  if (env === undefined) {
+    env = process.env.NODE_ENV
+  }
+
+  isProduction = env === 'production'
+
+  webpackConfig = {
+    // entry: {
+    //   // app: ['@babel/polyfill', './src/js/app.js']
+    // }, //If you need support IE11
+    output: {
+      filename: 'app.js'
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/js/')
+      }
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: '/node_modules/',
+          loader: 'eslint-loader',
+          options: {
+            fix: true,
+            cache: true,
+            ignorePattern: __dirname + '/src/js/lib/'
+          }
+        },
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: '/node_modules/',
+        },
+        {
+          test: /\.glsl$/,
+          loader: 'webpack-glsl-loader'
+        }
+      ]
+    },
+    mode: !isProduction ? 'development' : 'production',
+    devtool: !isProduction ?
+      '#cheap-module-eval-source-map' :
+      'none',
+    optimization: {
+      minimize: isProduction
+    },
+    plugins: [
+      new webpackReal.LoaderOptionsPlugin({
+        options: {
+          eslint: {
+            formatter: require('eslint-formatter-pretty')
+          }
+        }
+      }),
+      new webpackReal.NoEmitOnErrorsPlugin()
+    ]
+  }
+
+  if (isProduction) {
+    webpackConfig.plugins.push(
+      new webpackReal.LoaderOptionsPlugin({
+        minimize: true,
+      })
+    )
+    webpackConfig.plugins.push(
+
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: 5500,
+        openAnalyzer: false
+      })
+    )
+  }
+
+  return webpackConfig
+}
+
+module.exports = createConfig()
+module.exports.createConfig = createConfig
