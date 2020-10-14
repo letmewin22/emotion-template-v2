ca5507241813d2769cf91d0558e99b1d6f253f6d
letmewin22 <46356822+letmewin22@users.noreply.github.com>
2020-10-14 09:51:00 +0300

    add fonts preload in <head>

--- gulp/tasks/fontsInclude.js
+++ gulp/tasks/fontsInclude.js
@@ -5,8 +5,10 @@ const foldersName = require('../foldersName')
 function fontsStyle(cb) {
 
   const path = foldersName.sourceFolder + '/scss/helpers/fonts-include.scss'
+  const path2 = foldersName.sourceFolder + '/templates/partials/_fonts-include.html'
 
   fs.writeFile(path, '', cb)
+  fs.writeFile(path2, '', cb)
   return fs.readdir(config.build.fonts, function(err, items) {
     if (items) {
       let cFontname
@@ -15,6 +17,7 @@ function fontsStyle(cb) {
         fontname = fontname[0]
         if (cFontname !== fontname) {
           fs.appendFile(path, '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb)
+          fs.appendFile(path2, `<link rel="preload" href="./fonts/${fontname}.woff" type="font/woff" as="font" crossorigin="anonymous">\r\n`, cb)
         }
         cFontname = fontname
       }
